e5304894f873d47f7308f1dcc8c1f21b5f56b800
onlyblank <blankp@gmail.com>
2016-02-22 21:06:51 -0300

    Exercise number 2 resolved - Added README

---
+++ README.md
@@ -0,0 +1,35 @@
+# rankmi-test
+Para utilizar todos los ejercicios debes clonar el repositorio
+```{r, engine='bash', count_lines}
+$ git clone 'git@github.com:onlyblank/rankmi-test.git'
+$ cd rankmi-test
+```
+##Ejercicio 1
+
+##Ejercicio 2
+- Ir a la carpeta del ejercicio
+```{r, engine='bash', count_lines}
+$ cd  ejercicio2
+```
+- Correr node
+```{r, engine='bash', count_lines}
+$ node
+```
+<sup>***Debes tener instalado node***</sup>
+
+- Importar index.js
+
+```{r, engine='bash', count_lines}
+$ var getPosition = require "./index.js"
+```
+
+- Llamar la función con la coordenada deseada, por ejemplo "AB"
+
+```{r, engine='bash', count_lines}
+$ getPosition("AB")
+```
+<sup>***La entrada a la función debe contener sólo letras (a-z)***</sup>
+
+##Ejercicio 3
+
+##Ejercicio 4
--- ejercicio2/index.js
+++ ejercicio2/index.js
@@ -1,5 +1,23 @@
-// index.js
+function getPosition(coordenada){
+  // Set the alphabet array
+  var alphaArray = "abcdefghijklmnopqrstuvwxyz".split("");
+  // Set counter
+  var coordenadaArray; //Array to split the input
+  if(!(/^[a-zA-Z]+$/.test(coordenada.trim()))){
+    // In case the user doesn't must have only letters
+    throw "The input should contain only letters (a-z)"
+  }
+  /* 
+  Set the array to get each position value, erase the blank spaces and reverse it to calculate as a based 26 numeric system
+  */
+  coordenadaArray = coordenada.trim().split("").reverse();
+  // With the reduce method we accumulate the result in 'output' and return
+  return coordenadaArray.reduce(function(output, currentValue, index){
+    // The system doesn't have 0 so plus 1
+    position = alphaArray.indexOf(currentValue.toLowerCase()) + 1;
+    return output + Math.pow(alphaArray.length, index) * position;
+  }, 0);
+}
 
-// ## EJERCICIO 2 ##
-
-// Usando Javascript Crear un algoritmo que logre resolver, si se envía una coordenada de tipo "excel", ejemplo "ZA" nos devuelva el numero de columna a la que pertenece, ejemplo: Input: Z // Output: 26 Input: AA // Output: 27 Input: AB // Output: 28
+// Export the function to use it in console.
+module.exports =  getPosition;
