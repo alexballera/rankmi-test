979b15cf88163cf79a18cb7f99a9231daf0be222
onlyblank <blankp@gmail.com>
2016-02-22 22:24:28 -0300

    Exercise number 1 resolved - Updated README

--- README.md
+++ README.md
@@ -6,6 +6,39 @@ $ cd rankmi-test
 ```
 ##Ejercicio 1
 
+Optimización del código 
+
+```ruby
+
+def update_user user_data
+  begin  
+    if status
+      if user_role.present? && process_id.present? && user_id.present?
+        user_role_process_data = UserRoleProcess.where(user_id: user_id, role_id: user_role, enterprise_process_id: process_id)
+        if user_role_process_data.empty?
+          status = UserRoleProcess.create(user_id: user_id,role_id: user_role.to_i, enterprise_process_id: process_id.to_i)
+        end
+      end
+    end
+
+    if status
+      if area_id.present?
+        user_area = UserEnterpriseArea.where(user_id: user_id, enterprise_area_id: area_id.to_i).first
+
+        unless user_area.present?
+          status = UserEnterpriseArea.create(user_id: user_id, enterprise_area_id: area_id.to_i)
+        end
+      end
+    end
+
+    return status
+
+  rescue Exception => e
+    #Manejo de excepción
+  end
+end
+
+```
 ##Ejercicio 2
 - Ir a la carpeta del ejercicio
 ```{r, engine='bash', count_lines}
--- ejercicio1/update_user.rb
+++ ejercicio1/update_user.rb
@@ -1,49 +1,39 @@
+# Asumiendo que user_role process_id y user_id estan definidas o so
+
+def get_user_role_process
+  UserRoleProcess.find_by(user_id: user_id, role_id: user_role, enterprise_process_id: process_id)
+end
+
+def create_user_role_process
+  UserRoleProcess.create(user_id: user_id,role_id: user_role.to_i, enterprise_process_id: process_id.to_i)
+end
+
+def validate_role_process_and_user
+  user_role.present? && process_id.present? && user_id.present?
+end
+
+def validate_rol_process_id  
+  validate_role_process_and_user && (get_user_role_process || create_user_role_process)
+end
+
+# Asumiendo que area_id, user_id estan definidos
+def get_user_enterprise
+  UserEnterpriseArea.find_by(user_id: user_id, enterprise_area_id: area_id.to_i)
+end
+
+def create_user_enterprise
+  UserEnterpriseArea.create(user_id: user_id, enterprise_area_id: area_id.to_i)
+end
+
+def validate_area  
+  area_id.present? && (get_user_enterprise || create_user_enterprise)
+end
+
 def update_user user_data
-
-  begin
-  
-    if(!user_data[:id].nil?)
-
-      user = User.find(user_id)
-
-    else
-
-      user = User.new
-
-    end
-
-    status = user.save
-
-    if !status
-      return status
-    end
-
-    if status
-      if user_role.present? && process_id.present? && user_id.present?
-        user_role_process_data = UserRoleProcess.where(user_id: user_id, role_id: user_role, enterprise_process_id: process_id)
-
-            if user_role_process_data.empty?
-              status = UserRoleProcess.create(user_id: user_id,role_id: user_role.to_i, enterprise_process_id: process_id.to_i)
-        end
-      end
-    end
-
-    if status
-      if area_id.present?
-
-            user_area = UserEnterpriseArea.where(user_id: user_id, enterprise_area_id: area_id.to_i).first
-
-            unless user_area.present?
-              status = UserEnterpriseArea.create(user_id: user_id, enterprise_area_id: area_id.to_i)
-            end
-
-          end
-    end
-
-    return status
-
+  begin 
+    User.exists?(user_data[:id]) && validate_rol_process_id && validate_area        
   rescue Exception => e
     #Manejo de excepción
   end
-
 end
+
